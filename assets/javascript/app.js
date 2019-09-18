b6fa6e3e56c00178af21676fcf1e9c91eec69209
Jeff Greco <jeffpgreco@gmail.com>
2019-09-18 02:28:30 -0400

    fixed bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ var intervalId = null;
 
 
 
-$("#start").click(function () {
+$("#start").unbind().click(function () {
 
     $("#start").hide();
     for (i = 0; i < questions.question.length - 1; i++) {
